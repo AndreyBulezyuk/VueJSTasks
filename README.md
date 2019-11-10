347d0313e0212b9e3f0d2b8078884985c53321d7
Andrey Bulezyuk <andrey.income@gmail.com>
2019-11-10 22:19:42 +0100

    3 Instance Management

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # VueJSTasks
 
 This repository is a part of the VueJS Certificate Course by German IT Academy (https://git-academy.com).
+
+Corresponding Articles about code-lines in this repository:
+
+# Task 1 - Project Structure
+https://git-academy.com/vuejs-practice-1-project-structure/
+
+# Task 2 - Routing
+https://git-academy.com/vuejs-practice-routing/
+
+# Task 3 - Instance Management
+
+
+
+# Task 4 - 
+
+# Task 5 - 
+
+# Task 6 - 
+
---
+++ VueJS 2 Task - #3 Instance Management/vuejs2task3instancemanagement/src/components/ProductDetail.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="ProductDetail">
+      <h2>{{notification}}</h2>
+      <p>
+        Quantity: <input type="text" v-model="quantity"><br>
+        Total Value: {{ totalValue }}$
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetail',
+
+  data() {
+    return {
+      product: {
+        id: 1,
+        name: 'VueJS Certificate',
+        price: 999
+      },
+      quantity: 0,
+      // You could set 'notification' to desired default value 
+      // instead doing it trough life cycle hook 
+      notification: '' 
+    }
+  },
+
+  computed: {
+    totalValue: {
+      get() {
+        return this.quantity*this.product.price
+      }
+    }
+  },
+
+  beforeMount() {
+    if(this.quantity==0) {
+      this.notification = 'Please specify quantity!'
+    }
+  },
+
+  updated() {
+    if(this.quantity != 0) {
+      this.notification = 'Quantity updated!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  color:orange;
+  border: 1px solid orange;
+  width:50%;
+  margin: 0 25% 0 25%;
+}
+input {
+  height: 40px;
+  width: 70px;
+  font-size: 25px;
+}
+.ProductDetail {
+  text-align: center;
+}
+
+.ProductDetail p {
+  font-size: 30px;
+}
+</style>
